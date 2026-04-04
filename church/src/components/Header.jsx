30442a0ce846737b6783ade053d2cf8c6ceb0321
Caio Lopes <caiolopes871@gmail.com>
2026-04-04 14:20:50 -0300

    feat: Creates a header component

--- church/src/components/Header.jsx
+++ church/src/components/Header.jsx
@@ -1,8 +1,24 @@
+import logo from "../assets/logo.png";
+
 function Header() {
 
   return (
-    <header>
-      
+    <header className="header">
+      <img 
+        src={logo} 
+        alt="Igreja Cristã"
+        className="header__logo" 
+      />
+
+      <nav>
+        <ul className="header__list">
+          <li className="header__list-item">Início</li>
+          <li className="header__list-item">Sobre</li>
+          <li className="header__list-item">Cultos</li>
+          <li className="header__list-item">Mensagens</li>
+          <li className="header__list-item">Contato</li>
+        </ul>
+      </nav>
     </header>
   )
 }
